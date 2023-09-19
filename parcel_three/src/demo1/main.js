789ab317b56abc52550b3c55a3dff1ced013d01d
张悦 <caocl@isyscore.com>
2023-09-19 21:07:33 +0800

    --

--- parcel_three/src/demo1/main.js
+++ parcel_three/src/demo1/main.js
@@ -6,18 +6,21 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 //设置相机位置
+camera.position.x = 5;
+camera.position.z = 5;
 camera.position.set(0, 0, 10);
 
 scene.add(camera);
 
+const axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
+
 //添加物体
 //创建几何体
-const cubeGeometry = new THREE.BoxGeometry(1,1,1);
-const cubeMaterial = new THREE.MeshBasicMaterial({color: 0xffff00});
-
-//根据几何体和材质创建物体
-const tube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-scene.add(tube);
+const geometry = new THREE.BoxGeometry( 1, 1, 1 ); 
+const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} ); 
+const cube = new THREE.Mesh( geometry, material ); 
+scene.add( cube );
 
 const renderer = new THREE.WebGLRenderer();
 const width = 800; //宽度
